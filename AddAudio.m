f76df857949ee7b898593fad1c4763096b7d0ea5
kpisila <37945271+kpisila@users.noreply.github.com>
2020-03-31 01:37:48 -0400

    Added Cepstrum Analysis
    ReadAudio now performs a cepstrum analysis on the audio with the echo and shows a peak at the delay value. Graphs of two cepstrums are also now uploaded.

--- AddAudio.m
+++ AddAudio.m
@@ -1,9 +1,11 @@
 
-release(afr); 
-release(adw);
+
 %%%%%%%%%%%%%%%%%%%%% Create MATLAB objects to read the audio file %%%%%%%%
 afr1 = dsp.AudioFileReader('C:\Program Files\MATLAB\AudioFiles\The Car Song.mp3');
 afr2 = dsp.AudioFileReader('C:\Program Files\MATLAB\AudioFiles\The Car Song.mp3');
+
+%%%%%%%%%%%%%%%%%% Create MATLAB object to write a new audio file %%%%%%%%%
+afw = dsp.AudioFileWriter('C:\Users\Kaikki\Documents\GitHub\AudioWatermarking\OutputAudio\EchoEcho.wma', 'FileFormat', 'WMA');
 
 %%%%%%%%%%% Create MATLAB object to play the altered audio %%%%%%%%%%%%%%%%
 adw = audioDeviceWriter('SampleRate', afr2.SampleRate);
@@ -12,23 +14,30 @@ adw = audioDeviceWriter('SampleRate', afr2.SampleRate);
 %%%%%%%%%%%% returns the next 1024 x 2 array of the file   %%%%%%%%%%%%%%
 %%%%%%%%%%%% If one copy is called first before we start   %%%%%%%%%%%%%%
 %%%%%%%%%%%% The loop, it is ahead by one step.             %%%%%%%%%%%%%%
+
+delay = 512;    %%%%%%%% Alter this value to change the added delay
+
+afterDelay = delay + 1;
+startSecond = 1025 - delay;
+endFirst = 1024 - delay;
+
 counter = 0;
 audio2 = afr2();
 audio4 = zeros(1024,2);  %initialize temp matrix for splicing
 
-firsthalf = audio2(257:end,1:2);%take the second half of the first chunk
+firsthalf = audio2(afterDelay:end,1:2);%take the second half of the first chunk
 
 while counter < 500
-    audio4(1:768,1:2) = firsthalf;  %use it as the first half of the splice
+    audio4(1:endFirst,1:2) = firsthalf;  %use it as the first half of the splice
     
     audio1 = afr1();
     audio2 = afr2();
     
                                                 %use the first half of the
-    audio4(769:end,1:2) = audio2(1:256,1:2);    %second chunk as the second
+    audio4(startSecond:end,1:2) = audio2(1:delay,1:2);    %second chunk as the second
                                                 %half of the splice.
                                                 
-    firsthalf = audio2(257:end,1:2);%take the second half of the second 
+    firsthalf = audio2(afterDelay:end,1:2);%take the second half of the second 
                                     %chunk and save it for next time
                                     
     audio3 = (audio1 * 1) + (audio4 * .3);
@@ -37,6 +46,7 @@ while counter < 500
 %     end                   It is limited to multiples of the chunk size
  
     adw(audio3);            %play the audio chunk
+    afw(audio3);            %save the audio chunk
     counter = counter + 1;  %then move to the next chunk
 end
 afr1.reset();
@@ -46,5 +56,7 @@ while counter < 500
     adw(audio1);
     counter = counter +1;
 end
-release(afr); 
+release(afr1); 
+release(afr2); 
 release(adw);
+release(afw);
--- ReadAudio.m
+++ ReadAudio.m
@@ -1,9 +1,15 @@
-afr = dsp.AudioFileReader('04-Walk This Way.mp3');
-adw = audioDeviceWriter('SampleRate', afr.SampleRate);
-
-while ~isDone(afr)
-    audio = afr();
-    adw(audio);
+afr = dsp.AudioFileReader('C:\Users\Kaikki\Documents\GitHub\AudioWatermarking\OutputAudio\EchoEcho.wma');
+%adw = audioDeviceWriter('SampleRate', afr.SampleRate);
+audio = afr();
+counter = 0;
+while counter < 4 %~isDone(afr)
+    audio = [audio; afr()];
+    counter = counter + 1;
+  %  adw(audio);
 end
 release(afr); 
-release(adw);
+%release(adw);
+oneChannel = audio(1:4096,1:1);
+c = cceps(oneChannel);
+t = 1:1:4096;
+plot(t,c)
