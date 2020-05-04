28b48a296c31b73c8fe0ae8171fdfb357b32316a
bpoholcuhk <61512995+bpoholcuhk@users.noreply.github.com>
2020-05-04 17:27:51 -0400

    Test Audio
    each folder has the original track and the trimmed one minute tracks in mp3 and wav format

--- EchoWatermarking/readwatermark.m
+++ EchoWatermarking/readwatermark.m
@@ -4,12 +4,12 @@
    numChars = floor(BitLength / 7);
    numUsableBits = floor(numChars * 7); %%%%%%%%%%%%%%%%%%%%%%%%%%% Work in Progress Here
    truncated = opBitTemp(1:numUsableBits);
-   opBitTemp = num2str(truncated)% converts the string back to a char
+   opBitTemp = num2str(truncated);% converts the string back to a char
     
    %i = cell2mat(opBit);% creates a matrix from a cell
    
    %decoded = char(bin2dec(reshape(truncated,7,[]).')).';% reshapes the array into correct form and converts into characters
    
  
-disp('Your original word is:') 
+disp('Your original word is:');
 %disp(decoded) % displays word for checking process
