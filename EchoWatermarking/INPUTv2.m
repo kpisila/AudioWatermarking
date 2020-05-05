a45d8f1f3b7a3539ff9fec82b92e54b4e1538d90
kpisila <37945271+kpisila@users.noreply.github.com>
2020-05-05 00:45:55 -0400

    Ready for Testing

--- EchoWatermarking/INPUTv2.m
+++ EchoWatermarking/INPUTv2.m
@@ -1,9 +1,5 @@
-
 [bitstream , filepath, delay0, delay1] = addwatermark_v2();
 AddEchoFunction(bitstream, filepath, delay0, delay1);
     
 %C:\Program Files\MATLAB\AudioFiles\TheCarSong.mp3
-%C:\Users\Kai\Documents\GitHub\PerfectInput.wav
-
-
-    
+%C:\Users\Kai\Documents\GitHub\AudioWatermarking\EchoWatermarking\AudioTracks\Beethoven5th\TrimmedBeethoven5thSymphony.wav   
