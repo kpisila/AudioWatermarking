e0e17a7641c5379d3ce148b31b413a13c3af6f89
bpoholcuhk <61512995+bpoholcuhk@users.noreply.github.com>
2020-05-05 11:29:50 -0400

    fp

--- EchoWatermarking/v3/INPUT_v3.m
+++ EchoWatermarking/v3/INPUT_v3.m
@@ -3,3 +3,23 @@ AddEchoFunction_v3(bitstream, filepath, delay0, delay1);
     
 %C:\Program Files\MATLAB\AudioFiles\KillerQueen.mp3
 %C:\Users\Kai\Documents\GitHub\AudioWatermarking\EchoWatermarking\v3\InputAudio\IevanPolkka.mp3  
+
+%anvil
+%/Users/benpoholchuk/Documents/GitHub/AudioWatermarking/EchoWatermarking/v3/InputAudio/TrimmedLORN-ANVIL.wav
+
+
+
+
+
+%a will away
+%/Users/benpoholchuk/Documents/GitHub/AudioWatermarking/EchoWatermarking/v2/AudioTracks/AWillAway-HereAgain/TrimmedAWillAway-HereAgain.mp3
+
+%the menzingers
+%/Users/benpoholchuk/Documents/GitHub/AudioWatermarking/EchoWatermarking/v2/AudioTracks/TheMenzingers-NoTicket/TrimmedTheMenzingers-NoTicket.mp3
+
+%beethoven
+%/Users/benpoholchuk/Documents/GitHub/AudioWatermarking/EchoWatermarking/v2/AudioTracks/Beethoven5th/TrimmedBeethoven5thSymphony.mp3
+
+%the midnight
+%/Users/benpoholchuk/Documents/GitHub/AudioWatermarking/EchoWatermarking/v2/AudioTracks/TheMidnight-Vampires/TrimmedTheMidnight-Vampires.mp3
+
