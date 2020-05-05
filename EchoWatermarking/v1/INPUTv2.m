58c8875ef591d059b687527ec66c0792f183a86d
kpisila <37945271+kpisila@users.noreply.github.com>
2020-05-05 10:53:43 -0400

    inout

--- EchoWatermarking/v1/INPUTv2.m
+++ EchoWatermarking/v1/INPUTv2.m
@@ -1,5 +1,7 @@
 [bitstream , filepath, delay0, delay1] = addwatermark_v2();
 AddEchoFunction(bitstream, filepath, delay0, delay1);
+bitstreamOut = readWMAudio_v2('EchoWatermarkedTest.wav', delay0, delay1);
+err = immse(bitstream ,bitstreamOut)
     
 %C:\Program Files\MATLAB\AudioFiles\KillerQueen.mp3
 %C:\Users\Kai\Documents\GitHub\AudioWatermarking\EchoWatermarking\v2\AudioTracks\Anvil-Lorn\TrimmedLORN-ANVIL.mp3  
--- EchoWatermarking/v1/OUTPUT.m
+++ EchoWatermarking/v1/OUTPUT.m
@@ -1,6 +1,5 @@
 [userFilePath, delay0, delay1] = getUserFilepath();
 bitstream = readWMAudio_v2(userFilePath, delay0, delay1);
-readwatermark(bitstream); %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Not working, work in progress
-
+readwatermark(bitstream);
 
 %C:\Users\Kai\Documents\GitHub\EchoWatermarkedTest.wav
